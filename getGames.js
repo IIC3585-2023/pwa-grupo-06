3d4226532d49657477cc757a69ca1dded2c46dc7
agonzd <agonzd@uc.cl>
2023-05-22 01:28:03 -0400

    Cambios de credenciales de Firebase

--- getGames.js
+++ getGames.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebas
 import { initializeFirestore, collection, getDocs, persistentLocalCache } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpDnzNVueWQ3QiQg2qX3kpm1V1Pis2ufs",
-  authDomain: "pwa-grupo-06.firebaseapp.com",
-  projectId: "pwa-grupo-06",
-  storageBucket: "pwa-grupo-06.appspot.com",
-  messagingSenderId: "202038855165",
-  appId: "1:202038855165:web:56dd106586ac9c637ac594",
-  measurementId: "G-HW6F1KE7FE"
+  apiKey: "AIzaSyA-toGx8coidWYspBsuKWdIJuZp-dM_Pbs",
+  authDomain: "pwa-g6.firebaseapp.com",
+  projectId: "pwa-g6",
+  storageBucket: "pwa-g6.appspot.com",
+  messagingSenderId: "144479401328",
+  appId: "1:144479401328:web:020cf45f116d226d1e8bb5",
+  vapidKey: 'BLHZLAWEW1kvTgiPvKIb5dkmYgZACiBMHyO1_0PeIvWuzN8lZKLFK9LM1Z8BUzSzCchwc73C0ur4b5nSilJok18'
 };
 
 const app = initializeApp(firebaseConfig);
