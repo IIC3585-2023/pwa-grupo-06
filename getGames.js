10640c878c30b020e9aa41842de7cf012694dce1
Agustin <aiherrera1@uc.cl>
2023-05-22 10:14:04 -0400

    html with data

--- getGames.js
+++ getGames.js
@@ -1,5 +1,10 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
-import { initializeFirestore, collection, getDocs, persistentLocalCache } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import {
+  initializeFirestore,
+  collection,
+  getDocs,
+  persistentLocalCache,
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA-toGx8coidWYspBsuKWdIJuZp-dM_Pbs",
@@ -8,25 +13,71 @@ const firebaseConfig = {
   storageBucket: "pwa-g6.appspot.com",
   messagingSenderId: "144479401328",
   appId: "1:144479401328:web:020cf45f116d226d1e8bb5",
-  vapidKey: 'BLHZLAWEW1kvTgiPvKIb5dkmYgZACiBMHyO1_0PeIvWuzN8lZKLFK9LM1Z8BUzSzCchwc73C0ur4b5nSilJok18'
+  vapidKey:
+    "BLHZLAWEW1kvTgiPvKIb5dkmYgZACiBMHyO1_0PeIvWuzN8lZKLFK9LM1Z8BUzSzCchwc73C0ur4b5nSilJok18",
 };
 
 const app = initializeApp(firebaseConfig);
-const db = initializeFirestore(app, {localCache: persistentLocalCache(/*settings*/{})});
+const db = initializeFirestore(app, {
+  localCache: persistentLocalCache(/*settings*/ {}),
+});
 
 async function getGames() {
-  const gamesData = {}
+  const gamesData = {};
   const gamesQuerySnapshot = await getDocs(collection(db, "games"));
   gamesQuerySnapshot.forEach((doc) => {
     gamesData[doc.id] = doc.data();
   });
 
-  console.log(gamesData);
+  const gamesContainer = document.querySelector(".games"); // Get the games container element
+
+  for (const gameId in gamesData) {
+    const game = gamesData[gameId];
+
+    console.log(game);
+    // Create a game container element
+    const gameContainer = document.createElement("div");
+    gameContainer.classList.add("game");
+
+    // Add the game details to the game container
+    gameContainer.innerHTML = `
+      <div class="details">
+        <p class="fecha">${game.date}</p>
+      </div>
+      <div class="teams">
+        <div class="team">
+          <div class="details"></div>
+          <p class="name">${game.team_1}</p>
+          <p class="score">${game.score_1}</p>
+        </div>
+        <p class="vs">vs</p>
+        <div class="team">
+          <p class="name">${game.team_2}</p>
+          <p class="score">${game.score_2}</p>
+        </div>
+      </div>
+    `;
+    // Add onclick event listener to the game container
+    gameContainer.addEventListener("click", function () {
+      toggleText(this);
+    });
+    // Append the game container to the games container
+    gamesContainer.appendChild(gameContainer);
+  }
 
   // HACER AQUI LOS CAMBIOS EN EL HTML
-  
 
   return gamesData;
 }
 
+function toggleText(box) {
+  var boxContents = box.querySelectorAll(".score");
+  boxContents.forEach(function (content) {
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  });
+}
 getGames();
--- getTeams.js
+++ getTeams.js
@@ -1,5 +1,10 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
-import { initializeFirestore, collection, getDocs, persistentLocalCache } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import {
+  initializeFirestore,
+  collection,
+  getDocs,
+  persistentLocalCache,
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA-toGx8coidWYspBsuKWdIJuZp-dM_Pbs",
@@ -8,23 +13,42 @@ const firebaseConfig = {
   storageBucket: "pwa-g6.appspot.com",
   messagingSenderId: "144479401328",
   appId: "1:144479401328:web:020cf45f116d226d1e8bb5",
-  vapidKey: 'BLHZLAWEW1kvTgiPvKIb5dkmYgZACiBMHyO1_0PeIvWuzN8lZKLFK9LM1Z8BUzSzCchwc73C0ur4b5nSilJok18'
+  vapidKey:
+    "BLHZLAWEW1kvTgiPvKIb5dkmYgZACiBMHyO1_0PeIvWuzN8lZKLFK9LM1Z8BUzSzCchwc73C0ur4b5nSilJok18",
 };
 
 const app = initializeApp(firebaseConfig);
-const db = initializeFirestore(app, {localCache: persistentLocalCache(/*settings*/{})});
+const db = initializeFirestore(app, {
+  localCache: persistentLocalCache(/*settings*/ {}),
+});
 
 async function getTeams() {
-  const teamsData = {}
+  const teamsData = {};
   const teamsQuerySnapshot = await getDocs(collection(db, "teams"));
   teamsQuerySnapshot.forEach((doc) => {
     teamsData[doc.id] = doc.data();
   });
+  const sortedTeams = Object.entries(teamsData).sort(
+    (a, b) => b[1].points - a[1].points
+  );
 
+  const tableBody = document.querySelector("table tbody");
+
+  for (const [teamId, team] of sortedTeams) {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${team.name}</td>
+      <td>${team.points}</td>
+      <td>${team.played}</td>
+      <td>${team.wins}</td>
+      <td>${team.losses}</td>
+      <td>${team.draws}</td>
+    `;
+
+    tableBody.appendChild(row);
+  }
   console.log(teamsData);
-
-  // HACER AQUI LOS CAMBIOS EN EL HTML
-
 
   return teamsData;
 }
